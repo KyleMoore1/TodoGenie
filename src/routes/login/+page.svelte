1ca70b5f7cfb2057b328ee5ae71103c4896fcfb7
Kyle Moore <kylem70698@gmail.com>
2024-06-04 20:24:09 -0700

    adding sign up

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,60 +1,75 @@
-<form method="POST" action="?/login" class="max-w-sm mx-auto p-6 bg-white rounded-lg shadow-lg">
-	<div class="mb-4">
-		<label class="flex items-center gap-2">
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				viewBox="0 0 16 16"
-				fill="currentColor"
-				class="w-4 h-4 opacity-70"
-			>
-				<path
-					d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z"
-				/>
-				<path
-					d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z"
-				/>
-			</svg>
-			<input
-				name="email"
-				type="email"
-				placeholder="Email"
-				class="input input-bordered w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-			/>
-		</label>
-	</div>
-	<div class="mb-6">
-		<label class="flex items-center gap-2">
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				viewBox="0 0 16 16"
-				fill="currentColor"
-				class="w-4 h-4 opacity-70"
-			>
-				<path
-					fill-rule="evenodd"
-					d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
-					clip-rule="evenodd"
-				/>
-			</svg>
-			<input
-				name="password"
-				type="password"
-				placeholder="Password"
-				class="input input-bordered w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-			/>
-		</label>
-	</div>
-	<div class="flex justify-between">
-		<button
-			class="btn bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
+<section class="bg-gray-50 dark:bg-gray-900">
+	<div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+		<div
+			class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700"
 		>
-			Login
-		</button>
-		<button
-			class="btn bg-gray-500 text-white px-4 py-2 rounded-md hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-gray-500"
-			formaction="?/signup"
-		>
-			Sign up
-		</button>
+			<div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+				<h1
+					class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white"
+				>
+					Sign in to your account
+				</h1>
+				<form class="space-y-4 md:space-y-6" action="?/login" method="POST">
+					<div>
+						<label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+							>Your email</label
+						>
+						<input
+							type="email"
+							name="email"
+							id="email"
+							class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+							placeholder="name@company.com"
+							required
+						/>
+					</div>
+					<div>
+						<label
+							for="password"
+							class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label
+						>
+						<input
+							type="password"
+							name="password"
+							id="password"
+							placeholder="••••••••"
+							class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+							required
+						/>
+					</div>
+					<div class="flex items-center justify-between">
+						<div class="flex items-start">
+							<div class="flex items-center h-5">
+								<input
+									id="remember"
+									aria-describedby="remember"
+									type="checkbox"
+									class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
+								/>
+							</div>
+							<div class="ml-3 text-sm">
+								<label for="remember" class="text-gray-500 dark:text-gray-300">Remember me</label>
+							</div>
+						</div>
+						<a
+							href="#"
+							class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500"
+							>Forgot password?</a
+						>
+					</div>
+					<button
+						type="submit"
+						class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+						>Sign in</button
+					>
+					<p class="text-sm font-light text-gray-500 dark:text-gray-400">
+						Don’t have an account yet? <a
+							href="#"
+							class="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a
+						>
+					</p>
+				</form>
+			</div>
+		</div>
 	</div>
-</form>
+</section>
