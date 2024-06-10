e021b2f0291c88d858e678d2f41ee8ad1d27a545
Kyle Moore <kylem70698@gmail.com>
2024-06-10 13:01:25 -0700

    navbar initial commit

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,8 @@
-<script>
+<script lang="ts">
 	import '../app.css';
 	import { goto, invalidate } from '$app/navigation';
 	import { onMount } from 'svelte';
+	import { Button } from '$lib/components/ui/button/index.ts';
 
 	export let data;
 	$: ({ session, supabase } = data);
@@ -32,13 +33,35 @@
 	}
 </script>
 
-<div class="navbar bg-blue-100">
-	<div class="flex-1">
-		<a class="btn btn-ghost normal-case text-xl" href="/">TickTickClone</a>
+<nav class="fixed top-0 left-0 z-40 w-64 h-screen" aria-label="Sidebar">
+	<div
+		class="grid h-full px-3 py-4 overflow-y-auto grid-rows-[auto,1fr,auto] bg-gray-50 dark:bg-gray-800"
+	>
+		<div>
+			<ul class="space-y-2">
+				<li>
+					<Button href="/" class="w-full">All</Button>
+				</li>
+				<li>
+					<Button href="/" class="w-full" variant="ghost">Today</Button>
+				</li>
+				<li>
+					<Button href="/" class="w-full" variant="ghost">Next 7 Days</Button>
+				</li>
+				<li>
+					<Button href="/" class="w-full" variant="ghost">Inbox</Button>
+				</li>
+			</ul>
+		</div>
+		<div></div>
+		<div>
+			<ul class="space-y-2">
+				<li>
+					<Button class="w-full" variant="outline" on:click={handleLogout}>Logout</Button>
+				</li>
+			</ul>
+		</div>
 	</div>
-	<div class="flex-none">
-		<button class="btn btn-outline" on:click={handleLogout}>Logout</button>
-	</div>
-</div>
+</nav>
 
 <slot />
