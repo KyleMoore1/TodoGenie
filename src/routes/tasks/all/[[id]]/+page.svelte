3ca75bc23db4272dbabab7c9f4930f31f431ac8a
Kyle Moore <kylem70698@gmail.com>
2024-06-11 12:05:19 -0700

    todo items looking pretty good

--- src/routes/tasks/all/[[id]]/+page.svelte
+++ src/routes/tasks/all/[[id]]/+page.svelte
@@ -38,39 +38,22 @@
 			/>
 		</form>
 
-		<div class="flex flex-col items-center">
-			<ul class="list-none">
-				{#each data.todos as todo (todo.id)}
-					<li
-						class="bg-gray-100 border border-gray-300 rounded-md px-4 py-2 mb-2 shadow-sm flex items-center hover:bg-gray-200"
-					>
-						<input
-							type="checkbox"
-							class="mr-2"
-							bind:checked={todo.completed}
-							on:change={(e) => handleCheckboxChange(e, todo.id)}
-						/>
-						<span class="flex-grow">{todo.title}</span>
-						<button class="delete-button" on:click={() => handleDelete(todo.id)}>
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								class="h-6 w-6"
-								fill="none"
-								viewBox="0 0 24 24"
-								stroke="currentColor"
-							>
-								<path
-									stroke-linecap="round"
-									stroke-linejoin="round"
-									stroke-width="2"
-									d="M6 18L18 6M6 6l12 12"
-								/>
-							</svg>
-						</button>
-					</li>
-				{/each}
-			</ul>
-		</div>
+		<ul class="list-none">
+			{#each data.todos as todo (todo.id)}
+				<li
+					class="bg-gray-100 border border-gray-300 rounded-md px-4 py-2 mb-2 shadow-sm flex items-center hover:bg-gray-200 first:mt-5 mt-2.5"
+				>
+					<input
+						type="checkbox"
+						class="mr-2 h-4 w-4 accent-black border-gray-50 rounded"
+						bind:checked={todo.completed}
+						on:change={(e) => handleCheckboxChange(e, todo.id)}
+					/>
+					<span class="flex-grow">{todo.title}</span>
+					<button class="delete-button" on:click={() => handleDelete(todo.id)}>Delete</button>
+				</li>
+			{/each}
+		</ul>
 	</div>
 	<div class="bg-gray-50"></div>
 </div>
