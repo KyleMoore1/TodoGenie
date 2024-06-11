3f8f20d7f2bf48b3e56754b30ef97dca551eae22
Kyle Moore <kylem70698@gmail.com>
2024-06-11 09:08:10 -0700

    adding navbar

---
+++ src/lib/components/NavBar.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+	import { Button } from '$lib/components/ui/button/index.js';
+	export let data;
+	$: ({ supabase } = data);
+
+	function handleLogout() {
+		supabase.auth.signOut({
+			scope: 'local'
+		});
+	}
+</script>
+
+<nav class="fixed top-0 left-0 z-40 w-64 h-screen" aria-label="Sidebar">
+	<div
+		class="grid h-full px-3 py-4 overflow-y-auto grid-rows-[auto,1fr,auto] bg-gray-50 dark:bg-gray-800"
+	>
+		<div>
+			<ul class="space-y-2">
+				<li>
+					<Button href="/" class="w-full">All</Button>
+				</li>
+				<li>
+					<Button href="/" class="w-full" variant="ghost">Today</Button>
+				</li>
+				<li>
+					<Button href="/" class="w-full" variant="ghost">Next 7 Days</Button>
+				</li>
+				<li>
+					<Button href="/" class="w-full" variant="ghost">Inbox</Button>
+				</li>
+			</ul>
+		</div>
+		<div></div>
+		<div>
+			<ul class="space-y-2">
+				<li>
+					<Button class="w-full" variant="outline" on:click={handleLogout}>Logout</Button>
+				</li>
+			</ul>
+		</div>
+	</div>
+</nav>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,9 @@
 	import '../app.css';
 	import { goto, invalidate } from '$app/navigation';
 	import { onMount } from 'svelte';
-	import { Button } from '$lib/components/ui/button/index.ts';
+	import { Button } from '$lib/components/ui/button/index.js';
+	import { page } from '$app/stores';
+	import NavBar from '$lib/components/NavBar.svelte';
 
 	export let data;
 	$: ({ session, supabase } = data);
@@ -25,43 +27,10 @@
 
 		return () => data.subscription.unsubscribe();
 	});
-
-	function handleLogout() {
-		supabase.auth.signOut({
-			scope: 'local'
-		});
-	}
 </script>
 
-<nav class="fixed top-0 left-0 z-40 w-64 h-screen" aria-label="Sidebar">
-	<div
-		class="grid h-full px-3 py-4 overflow-y-auto grid-rows-[auto,1fr,auto] bg-gray-50 dark:bg-gray-800"
-	>
-		<div>
-			<ul class="space-y-2">
-				<li>
-					<Button href="/" class="w-full">All</Button>
-				</li>
-				<li>
-					<Button href="/" class="w-full" variant="ghost">Today</Button>
-				</li>
-				<li>
-					<Button href="/" class="w-full" variant="ghost">Next 7 Days</Button>
-				</li>
-				<li>
-					<Button href="/" class="w-full" variant="ghost">Inbox</Button>
-				</li>
-			</ul>
-		</div>
-		<div></div>
-		<div>
-			<ul class="space-y-2">
-				<li>
-					<Button class="w-full" variant="outline" on:click={handleLogout}>Logout</Button>
-				</li>
-			</ul>
-		</div>
-	</div>
-</nav>
+{#if !$page.url.pathname.startsWith('/auth')}
+	<NavBar {data} />
+{/if}
 
 <slot />
